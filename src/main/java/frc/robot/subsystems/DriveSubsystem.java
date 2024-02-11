7f818f129552089a00e719a54037f651ff7d6824
Amunr <amunreddy@gmail.com>
2024-02-10 18:14:03 -0800

    new file:   .gitignore 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .wpilib/wpilib_preferences.json 	new file:   WPILib-License.md 	new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/main/deploy/Swerve/controllerproperties.json 	new file:   src/main/deploy/Swerve/modules/backleft.json 	new file:   src/main/deploy/Swerve/modules/backright.json 	new file:   src/main/deploy/Swerve/modules/frontleft.json 	new file:   src/main/deploy/Swerve/modules/frontright.json 	new file:   src/main/deploy/Swerve/modules/physicalproperties.json 	new file:   src/main/deploy/Swerve/modules/pidfpropreties.json 	new file:   src/main/deploy/Swerve/swervedrive.json 	new file:   src/main/java/frc/robot/Constants.java 	new file:   src/main/java/frc/robot/Main.java 	new file:   src/main/java/frc/robot/Robot.java 	new file:   src/main/java/frc/robot/RobotContainer.java 	new file:   src/main/java/frc/robot/subsystems/DriveSubsystem.java 	new file:   vendordeps/NavX.json 	new file:   vendordeps/Phoenix6.json 	new file:   vendordeps/REVLib.json 	new file:   vendordeps/WPILibNewCommands.json 	new file:   vendordeps/yagsl.json

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,92 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+// import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import com.revrobotics.CANSparkMax;
+
+import java.util.function.DoubleSupplier;
+
+// import javax.management.RuntimeErrorException;
+
+/* 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import frc.robot.Constants.driveConstants;
+import com.revrobotics.RelativeEncoder;
+
+*/
+//swerve lib imports
+import java.io.File;
+// import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+import edu.wpi.first.math.util.Units;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+/* 
+import swervelib.SwerveController;
+import swervelib.SwerveDriveTest;
+import swervelib.math.SwerveMath;
+import swervelib.parser.SwerveControllerConfiguration;
+import swervelib.parser.SwerveDriveConfiguration;
+*/
+
+public class DriveSubsystem extends SubsystemBase {
+
+  public SwerveDrive swerveDrive;
+  public double maximumSpeed = Units.feetToMeters(18.5);
+  public DriveSubsystem(File directory){
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.NONE;
+    try {
+      swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    swerveDrive.setHeadingCorrection(false);
+    swerveDrive.setCosineCompensator(!SwerveDriveTelemetry.isSimulation);
+    
+  }
+
+  // Command to drive the robot using translative values and heading as a
+  // setpoint.
+  public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier headingX,
+      DoubleSupplier headingY) {
+    return run(() -> {
+      double xInput = Math.pow(translationX.getAsDouble(), 3); // Smooth controll out
+      double yInput = Math.pow(translationY.getAsDouble(), 3); // Smooth controll out
+      // Make the robot move
+      driveFieldOriented(swerveDrive.swerveController.getTargetSpeeds(xInput, yInput,
+          headingX.getAsDouble(),
+          headingY.getAsDouble(),
+          swerveDrive.getYaw().getRadians(),
+          swerveDrive.getMaximumVelocity()));
+    });
+  }
+
+  // Anuglar Velocity
+  public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY,
+      DoubleSupplier angularRotationX) {
+    return run(() -> {
+      // Make the robot move
+      swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumVelocity(),
+          translationY.getAsDouble() * swerveDrive.getMaximumVelocity()),
+          angularRotationX.getAsDouble() * swerveDrive.getMaximumAngularVelocity(),
+          true,
+          false);
+    });
+  }
+
+  public void driveFieldOriented(ChassisSpeeds velocity) {
+    swerveDrive.driveFieldOriented(velocity);
+  }
+
+  public void drive(ChassisSpeeds velocity){
+    swerveDrive.drive(velocity);
+  }
+}
